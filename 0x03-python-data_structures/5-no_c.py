c299a4ca730ef182513e7ece165f495276d25928
jynkeshie <jynkeshie@gmail.com>
2023-11-06 12:52:33 -0800

    updated 5-no_c.py file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for elements in my_string:
-        if elements != "c" and elements != "C":
-            new_string += elements
-            return new_string
+    # Create an empty string to store the result
+    result = ""
+    
+    # Iterate through the characters in the input string
+    for char in my_string:
+        # Check if the character is not 'c' or 'C'
+        if char != 'c' and char != 'C':
+            # Append the character to the result string
+            result += char
+    
+    return result
+
+if __name__ == "__main__":
+    print(no_c("Best School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
